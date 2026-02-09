8ab5e1a64ab9c45626e1c977d665c917a2882a42
ShakthiReign <shakthireign@gmail.com>
2026-02-10 00:07:28 +0530

    Added portfolio HTML,CSS and JS files

---
+++ script.js
@@ -0,0 +1,7 @@
+//JS file for my portfolio website
+
+let button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+    alert("Thank you for visiting my portfolio website!");
+}
